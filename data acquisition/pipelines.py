c99f045efc4807c8601adf7e1adf3cb9c7b2e09e
unknown <yunujang@naver.com>
2021-01-28 05:25:46 +0900

    connection with MongoDB

---
+++ data acquisition/pipelines.py
@@ -0,0 +1,14 @@
+import pymongo
+
+
+class MongoPipeline(object):
+    def __init__(self, mongo_uri, db_name, collection_name):
+        self.client = pymongo.MongoClient(mongo_uri)
+        self.db = self.client[db_name]
+        self.collection_name = collection_name
+
+    def __del__(self):
+        self.client.close()
+
+    def insert_item(self, item):
+        self.db[self.collection_name].insert(item)
