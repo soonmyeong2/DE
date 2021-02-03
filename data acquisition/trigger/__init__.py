20ead6cf9f8b24936a5dff81be8b6155cdd76ba4
unknown <yunujang@naver.com>
2021-02-03 23:11:46 +0900

    create a crawler trigger

---
+++ data acquisition/trigger/__init__.py
@@ -0,0 +1,29 @@
+import logging
+import grequests
+from pymongo import MongoClient
+import azure.functions as func
+
+
+def is_keyword_searched(keyword):
+    connection = MongoClient('your mongoDB')
+    db = connection['reviews']
+    collection = db[keyword]
+    return collection.estimated_document_count() != 0
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    keyword = req.params.get('keyword')
+    logging.info(f'Python HTTP trigger function processed a request. (keyword={keyword})')
+    
+    if is_keyword_searched(keyword):
+        return func.HttpResponse(f"this keyword has already been found.", status_code=200)
+
+    crawlers = [
+        '''
+        your crawler APIs
+        '''
+    ]
+
+    rs = (grequests.get(crawler, timeout=1) for crawler in crawlers)
+    grequests.map(rs)
+    return func.HttpResponse(f"crawler has run.", status_code=200)
