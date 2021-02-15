4e2d21d14bfba27db5bdfbd4235c016a2cb90658
unknown <yunujang@naver.com>
2021-02-15 17:23:06 +0900

    modified trigger for dashboard

--- data acquisition/trigger/__init__.py
+++ data acquisition/trigger/__init__.py
@@ -1,29 +1,43 @@
 import logging
 import grequests
 from pymongo import MongoClient
+from datetime import datetime
 import azure.functions as func
 
 
-def is_keyword_searched(keyword):
-    connection = MongoClient('your mongoDB')
-    db = connection['REVIEW_EARLY']
+def keyword_count(keyword, db):
+    collection = db['keyword']
+    collection.update_one({'keyword': keyword},
+                          {
+                              '$set': {'timeStamp': datetime.now()},
+                              '$inc': {'count': 1}
+                          },
+                          upsert=True)
+
+
+def is_keyword_searched(keyword, db):
     collection = db['reviews']
-    return bool(collection.find_one({"keyword": keyword}))
+    data = collection.find_one({"keyword": keyword})
+    try:
+        return (datetime.now() - data['timeStamp']).seconds < 3600 * 24
+    except:
+        return bool(data)
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     keyword = req.params.get('keyword')
+    connection = MongoClient('mongodb://id:pwd@host:port/dbname')
+    db = connection['REVIEW_EARLY']
     logging.info(f'Python HTTP trigger function processed a request. (keyword={keyword})')
-    
-    if is_keyword_searched(keyword):
-        return func.HttpResponse(f"this keyword has already been found.", status_code=200)
+
+    keyword_count(keyword, db)
+    if is_keyword_searched(keyword, db):
+        return func.HttpResponse("this keyword has already been found.", status_code=204)
 
     crawlers = [
-        '''
-        your crawler APIs
-        '''
+        # crawler API
     ]
 
     rs = (grequests.get(crawler, timeout=1) for crawler in crawlers)
     grequests.map(rs)
-    return func.HttpResponse(f"crawler has run.", status_code=200)
+    return func.HttpResponse(f"crawler has run.", status_code=201)
